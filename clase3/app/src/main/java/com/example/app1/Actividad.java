9f6f6fb1a01dc326e5c23918b52c03bb5d7cb2fe
Marco <soymarcochavez@hotmail.es>
2020-09-22 09:51:01 -0500

    kjnn

---
+++ clase3/app/src/main/java/com/example/app1/Actividad.java
@@ -0,0 +1,20 @@
+package com.example.app1;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Actividad extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        setContentView(R.layout.layout2);
+
+
+
+    }
+}
