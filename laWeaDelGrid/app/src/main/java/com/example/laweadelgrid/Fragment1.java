917cd36c9f675e6551bd2226e3a8de1b5d63448f
MarcoChavez <soymarcochavez@hotmail.es>
2020-09-29 22:20:59 -0500

    qowijoalsd

---
+++ laWeaDelGrid/app/src/main/java/com/example/laweadelgrid/Fragment1.java
@@ -0,0 +1,60 @@
+package com.example.laweadelgrid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+
+    int[] imagenes;
+
+    public void setImagenes(int[] imagenes) {
+        this.imagenes = imagenes;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment,container,false);
+
+        final int[] views = {
+                R.id.imageView0,
+                R.id.imageView1,
+                R.id.imageView2,
+                R.id.imageView3,
+                R.id.imageView4,
+                R.id.imageView5,
+                R.id.imageView6,
+                R.id.imageView7,
+                R.id.imageView8,
+                R.id.imageView9,
+                R.id.imageView10,
+                R.id.imageView11,
+        };
+
+        for (int i = 0; i < imagenes.length; i++) {
+
+            final ImageView img = v.findViewById(views[i]);
+            img.setImageResource(imagenes[i]);
+            img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(),String.valueOf(img.getId()),Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
+
+        return v;
+    }
+}
---
+++ laWeaDelGrid/app/src/main/java/com/example/laweadelgrid/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.laweadelgrid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Fragment1 frag1 = new Fragment1();
+        frag1.setImagenes(dameLegs());
+        getSupportFragmentManager().beginTransaction().add(R.id.cont1,frag1).commit();
+
+        Fragment1 frag2 = new Fragment1();
+        frag2.setImagenes(dameBody());
+        getSupportFragmentManager().beginTransaction().add(R.id.cont2,frag2).commit();
+
+        Fragment1 frag3 = new Fragment1();
+        frag3.setImagenes(dameHead());
+        getSupportFragmentManager().beginTransaction().add(R.id.cont3,frag3).commit();
+
+
+
+    }
+
+    public int[] dameBody(){
+        int[] bodys = {R.drawable.body1
+                , R.drawable.body2
+                , R.drawable.body3
+                , R.drawable.body4
+                , R.drawable.body5
+                , R.drawable.body6
+                , R.drawable.body7
+                , R.drawable.body8
+                , R.drawable.body9
+                , R.drawable.body10
+                , R.drawable.body11
+                , R.drawable.body12
+        };
+        return bodys;
+    }
+
+    public int[] dameLegs(){
+        int[] heads = {R.drawable.head1
+                , R.drawable.head2
+                , R.drawable.head3
+                , R.drawable.head4
+                , R.drawable.head5
+                , R.drawable.head6
+                , R.drawable.head7
+                , R.drawable.head8
+                , R.drawable.head9
+                , R.drawable.head10
+                , R.drawable.head11
+                , R.drawable.head12
+        };
+        return heads;
+    }
+
+    public int[] dameHead(){
+        int[] legs = {R.drawable.legs1
+                , R.drawable.legs2
+                , R.drawable.legs3
+                , R.drawable.legs4
+                , R.drawable.legs5
+                , R.drawable.legs6
+                , R.drawable.legs7
+                , R.drawable.legs8
+                , R.drawable.legs9
+                , R.drawable.legs10
+                , R.drawable.legs11
+                , R.drawable.legs12
+        };
+        return legs;
+    }
+
+}
